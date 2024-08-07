4852b573e31170f9207cd81ed797371686047ad9
Tsk-Andrey078 <tskandrey2002.@gmail.com>
2024-08-07 11:52:59 +0500

    Auth2

--- dev/web_core/web_api/urls.py
+++ dev/web_core/web_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import NavigationView, WidgetChildrenView, NavigationWidgetsView, NavigationChildrenView, WidgetView, FileUploadView, NavigationChildrenAllView
+from .views import NavigationView, WidgetChildrenView, NavigationWidgetsView, NavigationChildrenView, WidgetView, FileUploadView, NavigationChildrenAllView, GetNavigationHistory, GetWidgetHistory
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -12,6 +12,9 @@ urlpatterns = [
     path('widget/navigation/<int:id>', NavigationWidgetsView.as_view(), name="navigation_widgets"),
     path('navigation/<int:id>/children', NavigationChildrenView.as_view(), name="navigation-children"),
     path('navigation-children-all', NavigationChildrenAllView.as_view(), name="navigation-children-all"),
+    path('navigation-history', GetNavigationHistory.as_view(), name="navigation-history"),
+    path('widget-history', GetWidgetHistory.as_view(), name="widget-history"),
+    path('token/', obtain_auth_token, name='obtain_token'),
     path('', include(router.urls)),
     path('upload/', FileUploadView.as_view(), name='file-upload'),
 ]
